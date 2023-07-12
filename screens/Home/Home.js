a6833d5c8685041a24891d6327bd0683d80e7077
PremanandChowdhury <freudigprem@gmail.com>
2023-07-12 13:27:57 +0530

    Header Component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import globalStyle from '../../assets/style/globalStyle';
+
+import Header from '../../components/Header/Header';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <View>
-        <Text>Home here</Text>
-      </View>
+      <Header title={'Azzahri A. 👋'} type={1} />
     </SafeAreaView>
   );
 };
