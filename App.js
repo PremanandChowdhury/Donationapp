17f9dc7ac93d153bbe9d95bccfb4654bef7926df
Premanand Chowdhury <freudigprem@gmail.com>
2023-12-21 23:41:14 +0530

    lint changes

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import {PersistGate} from "redux-persist/integration/react";
-import store,  {persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistor} from './redux/store';
 import RootNavigation from './navigation/RootNavigation';
 
 const App = () => {
-  return <Provider store={store}>
+  return (
+    <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
         <NavigationContainer>
-        <RootNavigation />
+          <RootNavigation />
         </NavigationContainer>
       </PersistGate>
-  </Provider>;
+    </Provider>
+  );
 };
 
 export default App;
